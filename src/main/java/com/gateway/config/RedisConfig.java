bf8842d8055b4c588b3af8ea4b48cc41134be8e3
Yuvraj <parmaryuvrajsingh196@gmail.com>
2026-09-12 23:31:18 +0530

    add redis configuration

---
+++ src/main/java/com/gateway/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @Primary
+    public ReactiveRedisTemplate<String, String> reactiveStringRedisTemplate(
+            ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
+        StringRedisSerializer serializer = new StringRedisSerializer();
+        RedisSerializationContext<String, String> context = RedisSerializationContext
+                .<String, String>newSerializationContext(serializer)
+                .key(serializer)
+                .value(serializer)
+                .hashKey(serializer)
+                .hashValue(serializer)
+                .build();
+        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, context);
+    }
+}
